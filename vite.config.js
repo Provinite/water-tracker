572214efc79ad42b5b8101e62dd984a35fb57e1c
Collin Driscoll <provinite@gmail.com>
2026-02-06 11:40:01 -0600

    Auto-create .nojekyll file during build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'add-nojekyll',
+      closeBundle() {
+        writeFileSync('docs/.nojekyll', '')
+      }
+    }
+  ],
   base: '/water-tracker/',
   build: {
     outDir: 'docs',
